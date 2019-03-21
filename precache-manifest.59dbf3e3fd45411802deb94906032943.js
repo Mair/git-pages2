adfd8ffc5a5e2cf5adf67760ff469a1a60653bd7
mairs <mair.swartz@live.com>
2019-03-21 21:13:31 +1100

    Updates

---
+++ precache-manifest.59dbf3e3fd45411802deb94906032943.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e83387394d4aec6c9b5f",
+    "url": "/static/js/main.e9dd1343.chunk.js"
+  },
+  {
+    "revision": "42d16b51fdfc32c42197",
+    "url": "/static/js/2.3fb57f8a.chunk.js"
+  },
+  {
+    "revision": "e83387394d4aec6c9b5f",
+    "url": "/static/css/main.1d928507.chunk.css"
+  },
+  {
+    "revision": "13ba8b368d654ea00fc69a11197885ed",
+    "url": "/index.html"
+  }
+];
